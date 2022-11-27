a064ab2d939372227eb0c9e2f569d5a6da2a5a7c
dragekk <53555592+dragekk@users.noreply.github.com>
2022-11-27 22:21:43 +0100

    Another commit???

---
+++ README.md
@@ -0,0 +1,7 @@
+# blobcat/blobfox emoji generator with blanket
+simple website that generates blobcat or blobfox emoji with blanket that have any texture you want
+
+link:
+https://dragekk.github.io/blob-emoji-flag
+
+i made it because i was bored
--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const canvas2 = document.getElementById('canvas2');
 const context1 = canvas1.getContext('2d');
 const context2 = canvas2.getContext('2d');
 
-const blobcat = new Image(); blobcat.src = "blobcat.png";
-const blobfox = new Image(); blobfox.src = "blobfox.png";
+const rootContext = document.body.getAttribute("data-root");
+const blobcat = new Image(); blobcat.onload = function() {blobcat.crossOrigin = "Anonymous";}; blobcat.src = "blobcat.png";
+const blobfox = new Image(); blobfox.onload = function() {blobfox.crossOrigin = "Anonymous";}; blobfox.src = "blobfox.png";
+const blobcatoverlay = new Image(); blobcatoverlay.onload = function() {blobcatoverlay.crossOrigin = "Anonymous";}; blobcatoverlay.src = "blobcatoverlay.png";
+const blobfoxoverlay = new Image(); blobfoxoverlay.onload = function() {blobfoxoverlay.crossOrigin = "Anonymous";}; blobfoxoverlay.src = "blobfoxoverlay.png";
 
 function flagSelected() {
     const file = flagFile.files[0];
@@ -14,7 +17,7 @@ function flagSelected() {
     }
 
     
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.readAsDataURL(file);
 
     reader.onloadend = function() {
@@ -23,32 +26,72 @@ function flagSelected() {
         img.src = URL.createObjectURL(file);
 
         img.addEventListener('load', () => {
-            drawEmoji(blobcat, null, img, context1)
-            drawEmoji(blobfox, null, img, context2)
+            drawEmoji(blobcat, blobcatoverlay, img, context1)
+            drawEmoji(blobfox, blobfoxoverlay, img, context2)
         }, false);
     }
-         
-
-    /*
-    img.addEventListener('load', () => {
-        context.drawImage(img, 0, 0);
-        context.beginPath();
-        context.rect(5, 5, 100, 80);
-        context.stroke();
-        console.log("drawed");
-    }, false);
-    
-    img.scr = URL.createObjectURL(file);
-
-    console.log(img);
-    */
-    
 }
 
 flagFile.addEventListener("change", flagSelected, false);
 
+function lerpColor(a, b, t) {
+    return [
+        a[0] + (b[0] - a[0]) * t,
+        a[1] + (b[1] - a[1]) * t,
+        a[2] + (b[2] - a[2]) * t,
+        a[3] + (b[3] - a[3]) * t,
+    ];
+}
+
 function drawEmoji(background, overlay, flag, ctx) {
+    const pixelImageData = ctx.createImageData(1, 1);
+
     ctx.clearRect(0, 0, 128, 128);
-    ctx.drawImage(background, 0, 0);
-    ctx.drawImage(flag, 0, 0)
+
+    const backgroundCanvas = imageToCanvas(background);
+    const overlayCanvas = imageToCanvas(overlay);
+    const flagCanvas = imageToCanvas(flag);
+
+    const flagSize = Math.min(flag.width, flag.height);
+
+    for (let x = 0; x < 127; x++) {
+        for (let y = 0; y < 127; y++) {
+            let backgroundPixel = backgroundCanvas.getImageData(x, y, 1, 1).data;
+            let overlayPixel = overlayCanvas.getImageData(x, y, 1, 1).data;
+            let dx = (flag.width - flagSize) + overlayPixel[0] / 255 * flagSize;
+            let dy = overlayPixel[1] / 255 * flagSize;
+            let flagPixel = flagCanvas.getImageData(dx, dy, 1, 1).data; flagPixel[3] = 255;
+            let pixel = lerpColor(backgroundPixel, flagPixel, overlayPixel[3]/255);
+            if (backgroundPixel < 250) {
+                pixel = flagPixel;
+                pixel[3] = overlayPixel[3];
+            }
+            pixelImageData.data[0] = pixel[0];
+            pixelImageData.data[1] = pixel[1];
+            pixelImageData.data[2] = pixel[2];
+            pixelImageData.data[3] = pixel[3];
+
+            ctx.putImageData(pixelImageData, x, y);
+        }
+    }
+}
+
+function imageToCanvas(image) {
+    const canvas = document.createElement("CANVAS");
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(image, 0, 0);
+
+    return ctx;
+}
+
+function saveCanvas(name, canvas) {
+    const link = document.createElement('a');
+    link.download = name + '.png';
+    link.href = canvas.toDataURL();
+    link.click();
+    link.delete;
 }
